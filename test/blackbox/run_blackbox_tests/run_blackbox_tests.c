6df33433387b13adab276c466c72ae83c18fe83f
Lakshminarayana Gurram <38289425+lakshminarayanagurram@users.noreply.github.com>
2019-03-05 17:49:48 +0530

    Add blackbox test cases for submesh

--- test/blackbox/run_blackbox_tests/run_blackbox_tests.c
+++ test/blackbox/run_blackbox_tests/run_blackbox_tests.c
@@ -66,6 +66,10 @@
 
 #include "test_cases_channel_conn.h"
 #include "test_cases_get_all_nodes_by_dev_class.h"
+#include "test_cases_submesh01.h"
+#include "test_cases_submesh02.h"
+#include "test_cases_submesh03.h"
+#include "test_cases_submesh04.h"
 
 #include "../common/containers.h"
 #include "../common/common_handlers.h"
@@ -85,11 +89,12 @@ int main(int argc, char *argv[]) {
 
 	int failed_tests = 0;
 
-	/*failed_tests += test_meta_conn();
+	/*
+	failed_tests += test_meta_conn();
 	failed_tests += test_meshlink_set_status_cb();
 	failed_tests += test_meshlink_join();
 	failed_tests += test_meshlink_set_channel_poll_cb();
-	//  failed_tests += test_meshlink_channel_open_ex();
+	failed_tests += test_meshlink_channel_open_ex();
 	failed_tests += test_meshlink_channel_get_flags();
 	failed_tests += test_meshlink_set_channel_accept_cb();
 	failed_tests += test_meshlink_destroy();
@@ -126,8 +131,13 @@ int main(int argc, char *argv[]) {
 	failed_tests += test_meshlink_channel_open();
 	failed_tests += test_meshlink_channel_close();
 
-	failed_tests += test_meshlink_channel_conn();*/
+	failed_tests += test_meshlink_channel_conn();
 	failed_tests += test_optimal_pmtu();
+	*/
+	failed_tests += test_cases_submesh01();
+	failed_tests += test_cases_submesh02();
+	failed_tests += test_cases_submesh03();
+	failed_tests += test_cases_submesh04();
 
 	printf("[ PASSED ] %d test(s).\n", total_tests - failed_tests);
 	printf("[ FAILED ] %d test(s).\n", failed_tests);
