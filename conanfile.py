9d532c49ca3a888a817fa6754b5664ee5667baf7
Rohan Sahay <rohansahay@elear.solutions>
2020-01-22 15:45:32 +0530

    added version

--- conanfile.py
+++ conanfile.py
@@ -2,14 +2,17 @@ from conans import ConanFile, AutoToolsBuildEnvironment, tools
 
 class MeshlinklibConan(ConanFile):
     name = "meshlink"
+    version = "0.1.0"
     license = "<Put the package license here>"
     author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
+    url = "https://github.com/elear-solutions/meshlink"
     description = "This recipe file used to build and package binaries of meshlink repository"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
+    topics = ("peer-to-peer", "networking", "p2p")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
+    options = {
+        "shared": [True, False]
+        }
+    default_options ["shared"] = False
     generators = "make"
 
     def build(self):
