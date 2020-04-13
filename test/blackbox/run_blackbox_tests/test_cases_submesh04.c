a23a0e2d0333986b36c01813d9c72ba4240246a4
sairoop-elear <sairoop@elear.solutions>
2020-04-13 19:49:54 +0530

    Add test case for validating node get self submesh name

--- test/blackbox/run_blackbox_tests/test_cases_submesh04.c
+++ test/blackbox/run_blackbox_tests/test_cases_submesh04.c
@@ -26,6 +26,7 @@
 #include <setjmp.h>
 #include <cmocka.h>
 #include <assert.h>
+#include <linux/limits.h>
 #include "execute_tests.h"
 #include "test_cases_submesh04.h"
 #include "pthread.h"
@@ -33,6 +34,7 @@
 #include "../common/test_step.h"
 #include "../common/common_handlers.h"
 #include "../common/mesh_event_handler.h"
+#include "../../utils.h"
 
 #define CORENODE1_ID  "0"
 #define APP1NODE1_ID  "1"
@@ -40,10 +42,19 @@
 
 #define INIT_ST         0
 
+#define NUT                         "nut"
+#define PEER                        "peer"
+#define RELAY                       "relay"
+#define SUBMESH_NAME                "submesh"
+#define TEST_SUBMESH                "test_submesh"
+#define create_path(confbase, node_name, test_case_no)   assert(snprintf(confbase, sizeof(confbase), TEST_SUBMESH "_%ld_%s_%02d", (long) getpid(), node_name, test_case_no) > 0)
+
 static bool test_case_status = false;
 
 static void test_case_submesh_04(void **state);
 static bool test_steps_submesh_04(void);
+static void test_case_submesh_05(void **state);
+static bool test_steps_submesh_05(void);
 
 static char event_node_name[][10] = {"CORENODE1", "APP1NODE1", "APP1NODE2"};
 static const char *node_ids[] = { "corenode1", "app1node1", "app1node2" };
@@ -60,6 +71,9 @@ static black_box_state_t test_case_submesh_4_state = {
 	.test_case_name =  "test_cases_submesh04",
 	.node_names =  test_case_submesh_4_nodes,
 	.num_nodes =  3
+};
+static black_box_state_t test_case_submesh_5_state = {
+	.test_case_name = "test_cases_submesh05",
 };
 
 static int black_box_group0_setup(void **state) {
@@ -152,12 +166,135 @@ static bool test_steps_submesh_04(void) {
 	return true;
 }
 
+static struct sync_flag all_nodes_reachable_cond = {.mutex  = PTHREAD_MUTEX_INITIALIZER, .cond = PTHREAD_COND_INITIALIZER};
+
+static void meshlink_node_reachable_status_cb(meshlink_handle_t *mesh, meshlink_node_t *node, bool reachable_status) {
+	if(meshlink_get_self(mesh) == node) {
+		return;
+	}
+
+	assert_true(reachable_status);
+	static bool nut_status[2] = { 0 };
+	static bool peer_status[2] = { 0 };
+	static bool relay_status[2] = { 0 };
+
+	if(!strcasecmp(mesh->name, NUT)) {
+		if(!strcasecmp(node->name, PEER)) {
+            nut_status[0] = true;
+		} else if (!strcasecmp(node->name, RELAY)) {
+            nut_status[1] = true;
+		} else {
+            fail();
+		}
+	} else if(!strcasecmp(mesh->name, PEER)) {
+		if(!strcasecmp(node->name, NUT)) {
+            peer_status[0] = true;
+		} else if (!strcasecmp(node->name, RELAY)) {
+            peer_status[1] = true;
+		} else {
+            fail();
+		}
+	} else if(!strcasecmp(mesh->name, RELAY)) {
+		if(!strcasecmp(node->name, NUT)) {
+            relay_status[0] = true;
+		} else if (!strcasecmp(node->name, PEER)) {
+            relay_status[1] = true;
+		} else {
+            fail();
+		}
+	} else {
+        fail();
+	}
+
+	if(nut_status[0] && nut_status[1] && peer_status[0] && peer_status[1] && relay_status[0] && relay_status[1]) {
+        set_sync_flag(&all_nodes_reachable_cond, true);
+	}
+    return;
+}
+
+/* Execute SubMesh Test Case # 5 - Test whether a node can get it's self submesh handle */
+static void test_case_submesh_05(void **state) {
+	execute_test(test_steps_submesh_05, state);
+}
+
+static bool test_steps_submesh_05(void) {
+	char nut_confbase[PATH_MAX];
+	char peer_confbase[PATH_MAX];
+	char relay_confbase[PATH_MAX];
+	create_path(nut_confbase, NUT, 5);
+	create_path(peer_confbase, PEER, 5);
+	create_path(relay_confbase, RELAY, 5);
+
+	meshlink_set_log_cb(NULL, MESHLINK_DEBUG, log_cb);
+	meshlink_handle_t *mesh = meshlink_open(nut_confbase, NUT, TEST_SUBMESH, DEV_CLASS_STATIONARY);
+    assert_non_null(mesh);
+	meshlink_handle_t *mesh_peer = meshlink_open(peer_confbase, PEER, TEST_SUBMESH, DEV_CLASS_STATIONARY);
+    assert_non_null(mesh_peer);
+	meshlink_handle_t *mesh_relay = meshlink_open(relay_confbase, RELAY, TEST_SUBMESH, DEV_CLASS_BACKBONE);
+    assert_non_null(mesh_relay);
+
+    meshlink_set_node_status_cb(mesh, meshlink_node_reachable_status_cb);
+    meshlink_set_node_status_cb(mesh_peer, meshlink_node_reachable_status_cb);
+    meshlink_set_node_status_cb(mesh_relay, meshlink_node_reachable_status_cb);
+
+	link_meshlink_pair(mesh_relay, mesh_peer);
+
+	meshlink_submesh_t *submesh = meshlink_submesh_open(mesh_peer, SUBMESH_NAME);
+	assert_non_null(submesh);
+
+	char *invitation = meshlink_invite(mesh_peer, submesh, NUT);
+	assert_non_null(invitation);
+
+    assert_true(meshlink_start(mesh_peer));
+    assert_true(meshlink_start(mesh_relay));
+
+    assert_true(meshlink_join(mesh, invitation));
+
+    // Get submesh name of the self node
+
+    assert_non_null(meshlink_get_node_submesh(mesh, meshlink_get_self(mesh)));
+    assert_null(meshlink_get_node_submesh(mesh_peer, meshlink_get_self(mesh_peer)));
+    assert_null(meshlink_get_node_submesh(mesh_relay, meshlink_get_self(mesh_relay)));
+    assert_non_null(meshlink_get_node_submesh(mesh_peer, meshlink_get_node(mesh_peer, NUT)));
+
+    // Check whether all the node are reachable to each other in this topology
+
+    assert_true(meshlink_start(mesh));
+    assert_true(wait_sync_flag(&all_nodes_reachable_cond, 10));
+
+    assert_non_null(meshlink_get_node_submesh(mesh_relay, meshlink_get_node(mesh_relay, NUT)));
+
+    meshlink_set_node_status_cb(mesh, NULL);
+    meshlink_set_node_status_cb(mesh_peer, NULL);
+    meshlink_set_node_status_cb(mesh_relay, NULL);
+
+    // Cleanup
+
+    meshlink_close(mesh);
+    meshlink_close(mesh_peer);
+    meshlink_close(mesh_relay);
+    assert_true(meshlink_destroy(nut_confbase));
+    assert_true(meshlink_destroy(peer_confbase));
+    assert_true(meshlink_destroy(relay_confbase));
+	return true;
+}
+
 int test_cases_submesh04(void) {
+    int failed_test = 0;
 	const struct CMUnitTest blackbox_group0_tests[] = {
 		cmocka_unit_test_prestate_setup_teardown(test_case_submesh_04, setup_test, teardown_test,
 		                (void *)&test_case_submesh_4_state)
 	};
-	total_tests += sizeof(blackbox_group0_tests) / sizeof(blackbox_group0_tests[0]);
 
-	return cmocka_run_group_tests(blackbox_group0_tests, black_box_group0_setup, black_box_group0_teardown);
+	const struct CMUnitTest blackbox_group1_tests[] = {
+		cmocka_unit_test_prestate_setup_teardown(test_case_submesh_05, NULL, NULL,
+		                (void *)&test_case_submesh_5_state)
+	};
+	total_tests += (sizeof(blackbox_group0_tests) / sizeof(blackbox_group0_tests[0])) + (sizeof(blackbox_group1_tests) / sizeof(blackbox_group1_tests[0]));
+
+	failed_test += cmocka_run_group_tests(blackbox_group0_tests, black_box_group0_setup, black_box_group0_teardown);
+
+	failed_test += cmocka_run_group_tests(blackbox_group1_tests, NULL, NULL);
+
+	return failed_test;
 }
